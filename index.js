d04f3a9e058f668364123892856278a4aec00de1
typist <1003659191@qq.com>
2023-10-12 16:01:24 +0000

    feat: udpate default text

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 window.addEventListener('load', () => {
   const output = document.getElementById('output');
-
-  output.innerText = "<press any key>";
-
   document.body.addEventListener('keydown', (e) => {
     e.preventDefault();
     e.stopPropagation();
