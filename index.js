8375f81ee1239c687c7ee29848eafff130a54276
typist <1003659191@qq.com>
2023-10-24 15:55:24 +0000

    feat: display special keys

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+function parseSpecialKeys(e) {
+  const keys = [];
+
+  if (e instanceof KeyboardEvent) {
+    if (e.ctrlKey) {
+      keys.push('Control')
+    }
+    if (e.metaKey) {
+      keys.push('Meta')
+    }
+    if (e.altKey) {
+      keys.push('Alt')
+    }
+    if (e.shiftKey) {
+      keys.push('Shift')
+    }
+    return keys;
+  }
+
+  return keys
+}
+
 window.addEventListener('load', () => {
   const output = document.getElementById('output');
   document.body.addEventListener('keydown', (e) => {
     e.preventDefault();
     e.stopPropagation();
-    output.innerText = `"${e.key}"`
+    const specialKeys = parseSpecialKeys(e);
+    output.innerText = `${specialKeys.length > 0 ? specialKeys.join(' ') : ''} ${specialKeys.includes(e.key) ? '' : e.key}`
   })
 })
